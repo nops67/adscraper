3f9c31c1b619ea49b1f2b139e544050fd8a77ba2
nops67 <129905637+nops67@users.noreply.github.com>
2023-07-04 22:38:11 +0000

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,9 +1,15 @@
-#1.1
+# 1.3
 
 import requests
 from bs4 import BeautifulSoup
 from random import randint
-from time import sleep
+from urllib.parse import urlparse
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By                     
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 search = input("Search: ")
 
@@ -18,11 +24,11 @@ try:
         session = requests.Session()
         request = session.get(url, headers=headers)
         soup = BeautifulSoup(request.content, 'lxml')
-        for match in soup.find_all('script'):
-            if match.has_attr('src'):
-                if 'googlesyndication' in match['src']:
+        for script in soup.find_all('script'):
+            if script.has_attr('src'):
+                if 'googlesyndication' in script['src']:
                     try:
-                        key = match['src'].split('-')[2]
+                        key = script['src'].split('-')[2]
                         break
                     except IndexError:
                         pass
@@ -32,7 +38,20 @@ try:
                 key = "NO ADSENSE"
         print(url + " - " + key)
         with open(search + ".txt", 'a') as output:
-            output.write(url + " - " + key + "\n")
-        sleep(randint(2,5))
+            output.write(url + " - " + key)
+        
+            parsed_url = urlparse(url)
+            opts = Options()
+            opts.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:111.0) Gecko/20100101 Firefox/111.0")
+            browser = webdriver.Firefox(options=opts)
+            browser.get("https://www.similarweb.com/website/" + parsed_url.netloc)
+            try:
+                visits = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "engagement-list__item-value")))
+                global_rank = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "wa-rank-list__value")))
+            finally:
+                output.write(" - Total Visits: " + visits.text + " ; Global Rank: " + global_rank.text + "\n")
+                browser.implicitly_wait(randint(3, 5))
+                browser.close()
+
 except requests.ConnectionError:
     pass
